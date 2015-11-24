9b2f561a77d9b6686a877f0f5003d9c570fd31da
Tan Nguyen <tannv.dts@gmail.com>
2015-11-24 16:08:01 +0700

    update 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//POLICIES
 var policy_checkCookieToken=require("./api/policies/checkCookieToken");
 var policy_checkVersion=require("./api/policies/checkVersion");
 var policy_isActivated=require("./api/policies/isActivated");
@@ -11,6 +12,26 @@ var policy_isExternalPractitioner=require("./api/policies/isExternalPractitioner
 var policy_isInternalPractitioner=require("./api/policies/isInternalPractitioner");
 var policy_isPatient=require("./api/policies/isPatient");
 var policy_setExposeHeaders=require("./api/policies/setExposeHeaders");
+//POLICIES
+
+//SERVICE
+var service_ErrorWrap=require("./api/services/ErrorWrap");
+var service_HelperService=require("./api/services/HelperService");
+var service_RedisService=require("./api/services/RedisService");
+var service_RedisWrap=require("./api/services/RedisWrap");
+var service_UUIDService=require("./api/services/UUIDService");
+//SERVICE
+
+//RESPONSES
+var response_badRequest=require("./api/responses/badRequest");
+var response_forbidden=require("./api/responses/forbidden");
+var response_movedPermanently=require("./api/responses/movedPermanently");
+var response_notActivated=require("./api/responses/notActivated");
+var response_notFound=require("./api/responses/notFound");
+var response_ok=require("./api/responses/ok");
+var response_serverError=require("./api/responses/serverError");
+var response_unauthor=require("./api/responses/unauthor");
+//RESPONSES
 module.exports={
 	//POLICIES BEGIN
 	checkCookieToken:policy_checkCookieToken,
@@ -27,4 +48,23 @@ module.exports={
 	isPatient:policy_isPatient,
 	setExposeHeaders:policy_setExposeHeaders,
 	//POLICIES END
+	
+	//SERVICE BEGIN
+	ErrorWrap:service_ErrorWrap,
+	HelperService:service_HelperService,
+	RedisService:service_RedisService,
+	RedisWrap:service_RedisWrap,
+	UUIDService:service_UUIDService,
+	//SERVICE END
+	
+	//RESPONSES BEGIN
+	badRequest:response_badRequest,
+	forbidden:response_forbidden,
+	movedPermanently:response_movedPermanently,
+	notActivated:response_notActivated,
+	notFound:response_notFound,
+	ok:response_ok,
+	serverError:response_serverError,
+	unauthor:response_unauthor,
+	//RESPONSES END
 }
