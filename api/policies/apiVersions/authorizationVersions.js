478d7b8eb18e793f0c535c8287a4e2e5dad86cc2
Tan Nguyen <tannv.dts@gmail.com>
2015-11-25 15:22:21 +0700

    update 10

--- api/policies/apiVersions/authorizationVersions.js
+++ api/policies/apiVersions/authorizationVersions.js
@@ -1,12 +1,5 @@
-var moduleController_v0_1=require("../../controllers/Authorization/v0_1/ModuleController");
 var userRoleController_v0_1=require("../../controllers/Authorization/v0_1/UserRoleController");
 module.exports={
-	'get /api/module/GetModulesForUser':{
-		'0.1':{
-			enable:false,
-			action:moduleController_v0_1.GetModulesForUser
-		}
-	},
 
 	'post /api/user-role/CreateUserRoleWithExistUser':{
 		'0.1':{
