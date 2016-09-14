0d1999aa75bb57da6b92cbc124c788ceea524e74
tannvdts <tannv.dts@gmail.com>
2016-09-14 16:27:27 +0700

    update redis service

--- api/services/RedisService.js
+++ api/services/RedisService.js
@@ -2,10 +2,11 @@ var redis=require("./RedisWrap");
 var o=require("./HelperService");
 
 var userConnectKeyPrefix="userConnect:";
+var sessionConnectKeyPrefix = 'sessionConnect:';
 var concatp=function()
 {
 	var str="";
-	for (var i = 0; i < arguments.length; i++) 
+	for (var i = 0; i < arguments.length; i++)
 	{
 		if(o.checkData(arguments[i]))
 		{
@@ -17,29 +18,39 @@ var concatp=function()
 }
 
 module.exports={
+	pushSessionConnect: function(uid, sessionUser) {
+		var key=sessionConnectKeyPrefix+ uid;
+		redis.set(key, sessionUser);
+		return key;
+	},
+
+	getSessionConnect: function(key) {
+		return redis.get(key);
+	},
+
 	pushUserConnect:function(connectInfo)
 	{
 		o.exlog("REDIS SERVICE-> pushUserConnect");
 		var key=userConnectKeyPrefix+connectInfo.UserUID;
 		var hashKey=concatp(connectInfo.SystemType,connectInfo.DeviceID,connectInfo.AppID);
 		return redis.hget(key,hashKey)
-		.then(function(obj){
-			if(obj && obj.sid)
-			{
-				if(connectInfo.sid!=obj.sid)
-					redis.del('sess:'+obj.sid);
-				return "next";
-			}
-		},function(err){
-			throw err;
-		})
-		.then(function(data){
-			redis.hset(key,hashKey,connectInfo);
-			console.log("pushUserConnect:",key,hashKey,connectInfo);
-			return {status:'success'};
-		},function(err){
-			throw err;
-		})
+			.then(function(obj){
+				if(obj && obj.sid)
+				{
+					if(connectInfo.sid!=obj.sid)
+						redis.del('sess:'+obj.sid);
+					return "next";
+				}
+			},function(err){
+				throw err;
+			})
+			.then(function(data){
+				redis.hset(key,hashKey,connectInfo);
+				console.log("pushUserConnect:",key,hashKey,connectInfo);
+				return {status:'success'};
+			},function(err){
+				throw err;
+			})
 	},
 
 	removeUserConnect:function(connectInfo)
@@ -48,11 +59,11 @@ module.exports={
 		var key=userConnectKeyPrefix+connectInfo.UserUID;
 		var hashKey=concatp(connectInfo.SystemType,connectInfo.DeviceID,connectInfo.AppID);
 		return redis.hdel(key,hashKey)
-		.then(function(success){
-			return success;
-		},function(err){
-			throw err;
-		})
+			.then(function(success){
+				return success;
+			},function(err){
+				throw err;
+			})
 	},
 
 	getUserConnects:function(uid,connectInfo)
@@ -60,9 +71,9 @@ module.exports={
 		o.exlog("REDIS SERVICE-> getUserConnects");
 		var key=userConnectKeyPrefix+uid;
 		return redis.hkeysvals(key)
-		.then(function(vals){
-			console.log(vals);
-		})
+			.then(function(vals){
+				console.log(vals);
+			})
 	},
 
 	checkCurrentAccessWeb:function(uid)
@@ -74,22 +85,22 @@ module.exports={
 			error.pushError("uid.notProvided");
 			throw error;
 		}
-		
+
 		var key=userConnectKeyPrefix+uid;
 		var hashKey=concatp(o.const.systemType.website);
 
 		return redis.hget(key,hashKey)
-		.then(function(obj){
-			if(o.checkData(obj))
-			{
-				return {status:'exist'};
-			}
-			else
-			{
-				return {status:'notExist'};
-			}
-		},function(err){
-			throw err;
-		})
+			.then(function(obj){
+				if(o.checkData(obj))
+				{
+					return {status:'exist'};
+				}
+				else
+				{
+					return {status:'notExist'};
+				}
+			},function(err){
+				throw err;
+			})
 	}
 }
